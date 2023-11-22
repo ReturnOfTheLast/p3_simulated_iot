4e801c81842d72bc56a3f30a7fbb6194a67a9bf1
Aksel Andersen <akselporsborg@hotmail.com>
2023-11-22 11:43:52 +0100

    Small fixes and changes

--- surveillanceCamera.py
+++ surveillanceCamera.py
@@ -15,15 +15,15 @@ try:
         camera.resolution = (320, 240)
         camera.framerate = 24
         camera.rotation = 180
+
         # Start a preview and let the camera warm up for 2 seconds
         camera.start_preview()
         time.sleep(2)
+
         # Start recording, sending the output to the connection for 60
         # Seconds, then stop
         camera.start_recording(connection, format='h264')
         print("Started recording")
-        # camera.wait_recording(60)
-        # camera.stop_recording()
         while True:
             pass
 finally:
